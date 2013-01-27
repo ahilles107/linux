2654b031792da6ca0e0cdbaba0aea58c797ca1d2
mplonski <m.plonski@sokoli.pl>
2013-01-27 13:47:22 +0100

    displays fortune

---
+++ fortune.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function show_fortune {
+	RANGE=3
+	number=$RANDOM
+	let "number %= $RANGE"
+	case $number in
+		0)
+			cow="moose"
+			;;
+		1)
+			cow="tux"
+			;;
+		2)
+			cow="koala"
+			;;
+	esac
+
+	RANGE=2
+	number=$RANDOM
+	let "number %= $RANGE"
+	case $number in
+		0)
+			command="/usr/games/cowsay"
+			;;
+		1)
+			command="/usr/games/cowthink"
+			;;
+	esac
+	/usr/games/fortune | $command -f $cow
+}
+
+show_fortune
+
