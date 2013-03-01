24b206d6ba1bc62e35c1a6f2775e5566482b9b32
Maciej Plonski <m.plonski@sokoli.pl>
2013-03-01 21:17:37 +0100

    commit

--- display.sh
+++ display.sh
@@ -4,18 +4,23 @@
 # licence: GNU GPL
 #
 # changing screen resolution to 1920x1080 if there's
-# any screen connected using HDMI and to 1024x600
-# when it can't find any connected using HDMI
-# (useful for laptops when screen resolution
-# auto-set is not correctly working)
+# one connected using HDMI and to 1024x600 when it can't
+# find any connected (useful for laptops when screen
+# resolution auto-set is not correctly working)
 #
 # add to your desktop environment's autostart
 #
-xrandr |grep HDMI-0 |grep " connected "
+# Waring!
+# use xrandr to know how is your external montior named!
+# change LVDS to the name of the internal monitor and DFP1
+# to the name of the external monitor
+#
+
+xrandr |grep DFP1 |grep " connected "
 if [ $? -eq 0 ]; then
-    xrandr --output HDMI-0 --auto --mode 1920x1080
+    xrandr --output DFP1 --auto --mode 1920x1080
     xrandr --output LVDS --off
-    xrandr --output HDMI-0 --auto --mode 1920x1080
+    xrandr --output DFP1 --auto --mode 1920x1080
 else
     xrandr --output LVDS --mode 1024x600
 fi
