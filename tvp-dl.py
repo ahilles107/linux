9d0d0bbd5d584def594a98f11c5bea38d526f3c1
Maciej Plonski <m.plonski@sokoli.pl>
2013-11-04 00:04:04 +0100

    commit

--- tvp-dl.py
+++ tvp-dl.py
@@ -4,6 +4,10 @@
 
 import os, sys, urllib, json, re
 
+def quit(mess):
+    print (mess)
+    sys.exit()
+
 if len(sys.argv) < 2:
     print("Welcome to tvpw-dl!")
     print("\tUsage: %s url" % sys.argv[0])
@@ -15,33 +19,34 @@ print ("Let's start!")
 try:
     sock = urllib.urlopen(sys.argv[1])
     html = sock.read()
-    sock.close()
 except:
-    print ("Something is wrong... tvp.pl is not working!")
-    sys.exit()
+    quit ("Something is wrong... tvp.pl is not working!")
+else:
+    sock.close()
 
 p = re.compile('object_id:\'[0-9]+\'')
 k = p.search(html)
 
 if k.group() == None:
-    print ("Something is wrong... with tvp.pl! (or you)")
-    sys.exit()
+    quit ("Something is wrong... with tvp.pl! (or you)")
     
 num = k.group().split('\'')[1]
 
 try:
     sock = urllib.urlopen('http://www.tvp.pl/pub/stat/videofileinfo?video_id=%s' % num)
     html = sock.read()
+except:
+    quit ("Something is wrong... with tvp.pl!")
+else:
     sock.close()
 
+try:
     d = json.loads(html)
 except:
-    print ("Something is wrong... with tvp.pl!")
-    sys.exit()
+    quit ("Something is wrong... with tvp.pl!")
 
 if not d['file_name']:
-    print ("Something is wrong... with tvp.pl!")
-    sys.exit()
+    quit ("Something is wrong... with tvp.pl!")
 
 print ("Downloading...")
 
